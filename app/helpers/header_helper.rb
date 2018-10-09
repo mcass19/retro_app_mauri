3856992a697c3171b261342b8d70d92b07e7ac95
mcass19 <mcass19@hotmail.com>
2018-10-09 15:00:58 -0300

    Fix styles for now

--- app/helpers/header_helper.rb
+++ app/helpers/header_helper.rb
@@ -3,8 +3,8 @@
 # :nodoc:
 module HeaderHelper
   def show_header
-    unless (controller_name == 'home') && (action_name == 'index') 
-      render 'layouts/header'
-    end
+    return if (controller_name == 'home') && (action_name == 'index')
+
+    render 'layouts/header'
   end
 end
