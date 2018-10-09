ea6aa28b1d070a1124cf95f809f6d595b8aeead1
mcass19 <mcass19@hotmail.com>
2018-10-09 11:13:29 -0300

    Create new retrospectives's view with button to post topics

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :retrospectives, only: %i[index]
+  resources :retrospectives, only: %i[index new]
 end
--- spec/controllers/retrospective_controller_spec.rb
+++ spec/controllers/retrospective_controller_spec.rb
@@ -9,4 +9,11 @@ describe RetrospectivesController do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe 'GET #new' do
+    it 'returns http success' do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
