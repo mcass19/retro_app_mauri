06904bfff7dfb0a6972758ecf008b888fb40f1fd
mcass19 <mcass19@hotmail.com>
2018-10-19 10:41:59 -0300

    Add create method to the controller and the logic to store in the session what is being posted

--- app/controllers/retrospectives_controller.rb
+++ app/controllers/retrospectives_controller.rb
@@ -4,5 +4,29 @@
 class RetrospectivesController < ApplicationController
   def index; end
 
-  def new; end
+  def new
+    @to_mention = []
+    @to_discuss = []
+    set_session_params(@to_mention, @to_discuss)
+  end
+
+  def create
+    session_params
+    @to_mention << params[:topic_to_mention] if
+                                    params[:topic_to_mention].present?
+    @to_discuss << params[:topic_to_discuss] if
+                                    params[:topic_to_discuss].present?
+  end
+
+  private
+
+  def set_session_params(to_mention, to_discuss)
+    session[:array_to_mention] = to_mention
+    session[:array_to_discuss] = to_discuss
+  end
+
+  def session_params
+    @to_mention = session[:array_to_mention]
+    @to_discuss = session[:array_to_discuss]
+  end
 end
