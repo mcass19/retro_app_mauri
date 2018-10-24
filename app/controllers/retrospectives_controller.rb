130ba5180cff2dbc012ae74b04616ba43591727d
mcass19 <mcass19@hotmail.com>
2018-10-24 12:01:04 -0300

    Separate the methods of mention and discuss topics to have a better solution

--- app/controllers/retrospectives_controller.rb
+++ app/controllers/retrospectives_controller.rb
@@ -10,12 +10,22 @@ class RetrospectivesController < ApplicationController
     set_session_params(@to_mention, @to_discuss)
   end
 
-  def create
-    session_params
-    @to_mention << params[:topic_to_mention] if
-                                    params[:topic_to_mention].present?
-    @to_discuss << params[:topic_to_discuss] if
-                                    params[:topic_to_discuss].present?
+  def post_mention_topics
+    return if params[:topic_to_mention].blank?
+
+    mention_params
+    incoming_mention = params[:topic_to_mention]
+    @to_mention << incoming_mention
+    @new_mention = incoming_mention
+  end
+
+  def post_discuss_topics
+    return if params[:topic_to_discuss].blank?
+
+    discuss_params
+    incoming_discuss = params[:topic_to_discuss]
+    @to_discuss << incoming_discuss
+    @new_discuss = incoming_discuss
   end
 
   private
@@ -25,8 +35,13 @@ class RetrospectivesController < ApplicationController
     session[:array_to_discuss] = to_discuss
   end
 
-  def session_params
+  def mention_params
     @to_mention = session[:array_to_mention]
+    @new_mention = nil
+  end
+
+  def discuss_params
     @to_discuss = session[:array_to_discuss]
+    @new_discuss = nil
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  resources :retrospectives, only: %i[index new create]
+  resources :retrospectives, only: %i[index new]
+  resource :retrospective, only: [] do
+    post :post_mention_topics
+    post :post_discuss_topics
+  end
 end
