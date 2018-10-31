a2d6953eced49b60a00b4aa7beb6df3518f05725
mcass19 <mcass19@hotmail.com>
2018-10-31 14:12:13 -0300

    Rewrite the system tests in a more readable way

--- spec/system/home_page_spec.rb
+++ spec/system/home_page_spec.rb
@@ -3,16 +3,15 @@
 require 'rails_helper'
 
 describe 'HomePage' do
-  before do
-    driven_by :chrome
-  end
-
   it 'renders the image, text and button of the home page' do
     visit root_url
+
     expect(page).to have_css "img[src*='wye-logo']"
     expect(page).to have_text 'Welcome to WyeRetro App!'
     expect(page).to have_button 'Start'
+
     click_button 'Start'
+
     expect(page).to have_current_path(retrospectives_path)
   end
 end
